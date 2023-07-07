1954506fac4e041bec0aaa52090cd0b8341e717c
abdallh <Abdallh.kut18@gmail.com>
2023-07-07 14:39:46 +0300

    best seller item part 3

--- lib/Features/home/presentaion/views/widgets/home_view_body.dart
+++ lib/Features/home/presentaion/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
+import 'package:bookly/Features/home/presentaion/views/widgets/best_seller_list_view.dart';
 import 'package:bookly/Features/home/presentaion/views/widgets/featured_list_view.dart';
-import 'package:bookly/core/utils/assets.dart';
+
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'custom_app_bar.dart';
@@ -11,7 +12,7 @@ class HomeViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24),
+        padding: EdgeInsets.symmetric(horizontal: 30),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -24,39 +25,13 @@ class HomeViewBody extends StatelessWidget {
               "Best seller",
               style: Styles.textStyle18,
             ),
+            SizedBox(
+              height: 20,
+            ),
             BestSellerListView(),
           ],
         ),
       ),
-    );
-  }
-}
-
-class BestSellerListView extends StatelessWidget {
-  const BestSellerListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        SizedBox(
-          height: 120,
-          child: AspectRatio(
-            aspectRatio: 2.5 / 4,
-            child: Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                  color: Colors.red,
-                  image: const DecorationImage(
-                    fit: BoxFit.fill,
-                    image: AssetImage(
-                      AssetsData.testImage,
-                    ),
-                  )),
-            ),
-          ),
-        ),
-      ],
     );
   }
 }
