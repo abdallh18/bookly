d81f7ad65a0d82ee2a653f0b0864b7b0ed3ac515
abdallh <Abdallh.kut18@gmail.com>
2023-07-07 10:05:58 +0300

    creat app styles

--- lib/Features/home/presentaion/views/widgets/home_view_body.dart
+++ lib/Features/home/presentaion/views/widgets/home_view_body.dart
@@ -1,5 +1,4 @@
 import 'package:bookly/Features/home/presentaion/views/widgets/featured_list_view.dart';
-import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -5,4 +5,20 @@ abstract class Styles {
     fontSize: 18,
     fontWeight: FontWeight.w600,
   );
+  static const textStyle20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+  );
+  static const textStyle14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+  static const textStyle16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+  static const textStyle30 = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.normal,
+  );
 }
